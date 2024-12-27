3ddfcddac16cbbbbbd4c3052304d4edd32c038d0
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-12-27 13:47:55 -0600

    chore: add todo

--- src/EvalLab.Client/src/views/EvaluationView.vue
+++ src/EvalLab.Client/src/views/EvaluationView.vue
@@ -4,6 +4,17 @@
   import { useService } from '../composables/useService.ts';
   import { EvaluationsServiceKey } from '../services/evaluationService.ts';
 
+  // TODO: Evaluations need further defining
+  // user needs to select the target pipeline of the evaluation
+  // user needs to select the criteria for the evaluation
+  // - exact match
+  // - partial match
+  // - cosine similarity
+  // - LLM assisted => could possibly be stored "graders" or "evaluators"
+  // user needs to provide the expected output
+  // user needs to provide the output type i.e. structured or unstructured
+  // user needs to provide number of runs to evaluate
+
   const route = useRoute();
   const evaluationId = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
   const data = ref<string>('');
