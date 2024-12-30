efd279f859408c944fadf9b4269d15bae38b5904
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-12-30 11:30:57 -0600

    chore: add todo with psuedo code

--- src/EvalLab.API/Evaluations/TestRunProcessor.cs
+++ src/EvalLab.API/Evaluations/TestRunProcessor.cs
@@ -15,6 +15,15 @@ class TaskRunProcessor(
 
   public async Task ProcessAsync(TestRunQueueItem item)
   {
+    // TODO: Implement the processing logic here
+    // retrieve the evaluation run
+    // retrieve the evaluation
+    // retrieve the target pipeline
+    // provide evaluation input to the pipeline
+    // run the pipeline
+    // apply evaluation success criteria to the pipeline output
+    // add a test run to the evaluation run
+    // save the evaluation run
     await Task.Delay(100);
     _logger.LogInformation("Processing sample {SampleNumber} for evaluation run {EvaluationRunId}", item.SampleNumber, item.EvaluationRunId);
   }
